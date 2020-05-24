f618b30eef5fe38d66901bc5442578d5a6d4ceee
Abdullah Nassif <AbNassif99@gmail.com>
2020-05-24 05:39:20 +0300

    Updates

---
+++ precache-manifest.d4d9bd94d826b2c1da6ee0a383817636.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9716b6af8affe0ac6610b883f9aa4cdb",
+    "url": "/RoboApp-Practice/index.html"
+  },
+  {
+    "revision": "6c697a8cb99b71c073cb",
+    "url": "/RoboApp-Practice/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "41ae3f72e572be0c2f17",
+    "url": "/RoboApp-Practice/static/css/main.c57edc71.chunk.css"
+  },
+  {
+    "revision": "6c697a8cb99b71c073cb",
+    "url": "/RoboApp-Practice/static/js/2.ece5dba7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RoboApp-Practice/static/js/2.ece5dba7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41ae3f72e572be0c2f17",
+    "url": "/RoboApp-Practice/static/js/main.4ebc31c6.chunk.js"
+  },
+  {
+    "revision": "9d197f0828b87ed143f4",
+    "url": "/RoboApp-Practice/static/js/runtime-main.29e3416d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboApp-Practice/static/media/SEGA.12a3db8b.woff"
+  }
+]);
